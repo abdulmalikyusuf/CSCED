bdf1d2ddc6aafdeca810a77164f53f2181c49b98
Abdulmalik Yusuf <31839174+abdulmalikyusuf@users.noreply.github.com>
2024-07-15 21:48:22 +0100

    fix: changed partners marquee component

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable prefer-const */
 // @ts-nocheck
 
-import { useRef } from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import Marquee from "react-fast-marquee";
+
 import AdamawaSACA from "@/assets/images/partners/adamawa-SACA.jpg";
 import BritishCouncil from "@/assets/images/partners/british-council.png";
 import FAO from "@/assets/images/partners/fao.jpg";
@@ -14,109 +13,13 @@ import UNHCR from "@/assets/images/partners/unhcr.png";
 import WorldBank from "@/assets/images/partners/world-bank.jpg";
 
 function Partners() {
-  const container = useRef<HTMLDivElement | null>(null);
-  const ref = useRef<HTMLDivElement[]>([]);
-
-  const pushRef = (el: HTMLDivElement | null) => {
-    if (el && !ref.current.includes(el)) {
-      ref.current.push(el);
-    }
-  };
-
-  useGSAP(
-    () => {
-      let items = gsap.utils.toArray<HTMLDivElement>(ref.current);
-
-      let config = {
-        paused: false,
-        repeat: -1,
-        snap: 1,
-        speed: 1,
-        paddingRight: 0,
-      };
-      let tl = gsap.timeline({
-          repeat: config.repeat,
-          paused: config.paused,
-          defaults: { ease: "none" },
-          onReverseComplete: () =>
-            tl.totalTime(tl.rawTime() + tl.duration() * 100),
-        }),
-        length = items.length,
-        startX = items[0].offsetLeft,
-        times = [],
-        widths = [],
-        xPercents = [],
-        pixelsPerSecond = (config.speed || 1) * 100,
-        snap =
-          config.snap === false ? (v) => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
-        totalWidth,
-        curX,
-        distanceToStart,
-        distanceToLoop,
-        item,
-        i;
-      gsap.set(items, {
-        // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-        xPercent: (i, el) => {
-          let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
-          xPercents[i] =
-            (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
-            gsap.getProperty(el, "xPercent");
-          console.log(xPercents);
-
-          return xPercents[i];
-        },
-      });
-      gsap.set(items, { x: 0 });
-      totalWidth =
-        items[length - 1].offsetLeft +
-        (xPercents[length - 1] / 100) * widths[length - 1] -
-        startX +
-        items[length - 1].offsetWidth *
-          gsap.getProperty(items[length - 1], "scaleX") +
-        (parseFloat(config.paddingRight) || 0);
-
-      for (i = 0; i < length; i++) {
-        item = items[i];
-        curX = (xPercents[i] / 100) * widths[i];
-        distanceToStart = item.offsetLeft + curX - startX;
-        distanceToLoop =
-          distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
-
-        tl.to(
-          item,
-          {
-            xPercent: -50,
-            duration: distanceToLoop / pixelsPerSecond,
-          },
-          0
-        )
-          .fromTo(
-            item,
-            {
-              xPercent: snap(
-                ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
-              ),
-            },
-            {
-              xPercent: xPercents[i],
-              duration:
-                (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
-              immediateRender: false,
-            },
-            distanceToLoop / pixelsPerSecond
-          )
-          .add("label" + i, distanceToStart / pixelsPerSecond);
-        times[i] = distanceToStart / pixelsPerSecond;
-      }
-      tl.progress(1, true).progress(0, true); // pre-render for performance
-      tl.play();
-    },
-    { scope: container.current }
-  );
-
   return (
-    <div className="flex gap-8 *:shrink-0 overflow-x-hidden" ref={container}>
+    <Marquee
+      pauseOnHover
+      pauseOnClick
+      direction="left"
+      className="flex gap-8 *:shrink-0 overflow-x-hidden"
+    >
       {[
         AdamawaSACA,
         BritishCouncil,
@@ -127,12 +30,7 @@ function Partners() {
         UNHCR,
         WorldBank,
       ].map((partner) => (
-        <div
-          key={partner}
-          ref={pushRef}
-          // ref={(el: HTMLDivElement) => (ref.current[idx] = el)}
-          className="marquee__item"
-        >
+        <div key={partner} className="marquee__item">
           <img
             src={partner}
             alt=""
@@ -140,7 +38,7 @@ function Partners() {
           />
         </div>
       ))}
-    </div>
+    </Marquee>
   );
 }
 
